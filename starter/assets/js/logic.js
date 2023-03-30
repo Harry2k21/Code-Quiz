7dcbc3db665a830f85a198e91e09a33520866055
unknown <parker2002hp@gmail.com>
2023-03-30 05:08:52 +0100

    added timer

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,7 +1,47 @@
-document.addEventListener('clcik');{
-    console.log("Start");
+// document.addEventListener('clcik');{
+//     console.log("Start");
+// }
+// var start = false
+var start = document.querySelector("#start")
+if(start == true){
+    display(question1)
 }
 
-if('#start' == true){
-    display(question1)
+
+start.addEventListener("click", question);{
+    console.log("test");
+}
+
+document.querySelector("#start").addEventListener("click", func1);
+
+// hides start screen
+function func1() {
+    document.getElementById("start").innerHTML = "Clicked";
+    document.getElementById("start-screen").style.display = "none";
+    // document.getElementById("start").innerHTML = "Start";
+}
+
+//timer
+var timeEl = document.getElementById("time")
+var timerCount = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      timerCount--;
+      timeEl.textContent = timerCount + " seconds left till game ends.";
+  
+      if(timerCount === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+setTime();
+function sendMessage() {
+    console.log("End Game");
 }
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,12 +1,30 @@
 var question = [
     
-    {question: "How do you write a for loop?",
+    {question1: "How do you write a for loop?",
     answer1: "for(loop)",
 answer2: "forfor",
 answer3: "loop(for)",
 answer4: "for(var i = 0; i < var.length; i++){};"},
 
     {question2: "Which is the lanaguage that adds logic to an application",
+    answer1: "HTML",
+    answer2: "CSS",
+    answer3: "Javascript",
+    answer4: "Git"},
+
+    {question3: "Which is the lanaguage that adds logic to an application",
+    answer1: "for(loop)",
+    answer2: "forfor",
+    answer3: "loop(for)",
+    answer4: "for(var i = 0; i < var.length; i++){};"},
+
+    {question4: "Which is the lanaguage that adds logic to an application",
+    answer1: "for(loop)",
+    answer2: "forfor",
+    answer3: "loop(for)",
+    answer4: "for(var i = 0; i < var.length; i++){};"},
+
+    {question5: "Which is the lanaguage that adds logic to an application",
     answer1: "for(loop)",
     answer2: "forfor",
     answer3: "loop(for)",
